5430357c5d5d449b8ddc9a5116476c5e1dd205bc
gone-still <eldesgraciado@gmail.com>
2023-02-03 18:07:57 -0600

    Added 004

---
+++ 004 - listMedian/listMedian.py
@@ -0,0 +1,35 @@
+# File        :   listMedian.py
+# Version     :   1.0.0
+# Description :   Solution to the listMedian problem
+#                
+# Date:       :   Feb 03, 2023
+# Author      :   Mr. X
+# License     :   Creative Commons CC0
+
+# Compute the median of a list of
+# numbers.
+
+import math
+
+# List of numbers:
+numbersList = [1, 2, 3, 4]
+# Get list size:
+listSize = len(numbersList)
+# Sort from smallest to largest:
+sortedList = numbersList.sort()
+
+# Get middle (floored)
+middle = listSize // 2
+
+# Check list size:
+if (listSize % 2 == 0):
+    # If even, median is average between the two
+    # middle elements:
+    median = numbersList[middle] + numbersList[middle-1]
+    median = median / 2
+else:
+    # If odd, median is just the central element:
+    median = numbersList[middle]
+    
+# Done:
+print(median)
