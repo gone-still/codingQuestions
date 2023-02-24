5526838d24ee9c9ab470f89ab54ddb7c1e0548c3
gone-still <eldesgraciado@gmail.com>
2023-02-24 01:11:22 -0600

    Updated 018

--- 018 - pickingNumbers/pickingNumbers.py
+++ 018 - pickingNumbers/pickingNumbers.py
@@ -1,8 +1,8 @@
 # File        :   pickingNumbers.py
-# Version     :   1.0.0
+# Version     :   1.5.0
 # Description :   Solution to the pickingNumbers problem
 #                
-# Date:       :   Feb 23, 2023
+# Date:       :   Feb 24, 2023
 # Author      :   Mr. X
 # License     :   Creative Commons CC0
 
@@ -19,6 +19,7 @@ from collections import Counter
 
 data = [4, 6, 5, 3, 3, 1]
 
+
 # Get a count of the items:
 countDict = Counter(data)
 # Sort counter by keys, from smallest to largest:
@@ -26,12 +27,23 @@ countDict = dict(sorted(countDict.items()))
 # Get keys list:
 dictKeys = list(countDict.keys())
 
-# Loop through the keys list, geta pair of
+# The subarray can have multiple occurrences of
+# the same item, test all the number against 
+# themselves, duplicate the entries:
+dictKeys = dictKeys + dictKeys
+
+# Sort again, from small to large:
+dictKeys.sort()
+
+# Get total pairs:
+totalPairs = len(dictKeys)
+# Prepare subarray:
+subArray = []
+maxLength = 0
+
+# Loop through the keys list, get a pair of
 # entries, compute their absolute difference
 # and maximum length:
-
-maxLength = 0
-subArray = []
 
 for i in range(len(dictKeys)-1):
     
@@ -46,16 +58,29 @@ for i in range(len(dictKeys)-1):
     aCount = countDict[a]
     bCount = countDict[b]
     
-    # Compute subarray length fo
+    # Compute subarray length for
     # this pair:
     l = aCount + bCount
     
+    print((a,b,aCount,bCount,d,l))
+    
     # Difference must be less than or equal to 1.
     # SubArray most be of maximum length:
-    if (d <= 1) and (l > maxLength):
-        maxLength = l
-        subArray = [a]*aCount + [b]*bCount
-        
+    if (d <= 1):
+        # Only same items can have distance of 0,
+        # adjust subarray length to remove the duplicated
+        # item count:
+        if (d == 0):
+            l = 0.5 * l   
+        # If pair lenght > 0, you are testing two different
+        # items and subarray length should not be adjusted:         
+        if (l > maxLength): 
+            maxLength = l     
+
+# The practicing website gives wrong score if
+# the result is not an integer (LOL):
+maxLength = int(maxLength)
+    
 # Print the results:   
 print(maxLength)
 print(subArray)
