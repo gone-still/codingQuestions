a5955e15a3de80d4c7df6a5eb33318ef33418cc5
gone-still <eldesgraciado@gmail.com>
2023-03-02 14:24:29 -0600

    Update sockSorting.cpp

--- 022 - sockSorting/sockSorting.cpp
+++ 022 - sockSorting/sockSorting.cpp
@@ -11,9 +11,7 @@
 
 
 // Example:
-
 // sockColors = [ 1, 2, 1, 2, 1, 3, 2, 3 ]
-
 // There are 3 pairs of socks and 2 singles.
 
 
@@ -76,7 +74,7 @@ int main()
             }
         }
 
-        // Checck out the Data so far:
+        // Checck out the data so far:
         std::cout<<"Pairs: "<<sockPairs<<std::endl;
         std::cout<<"Singles: "<<sockSingles<<std::endl;
     }
